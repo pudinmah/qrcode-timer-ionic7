ad0a699eeb9f4bf770c04f20c1ddb25c34a22616
pudinmah <pudinmah69@gmail.com>
2024-08-30 04:04:15 +0700

    menampilkan timer 15 detik bersama dengan QR code yang berubah

--- src/app/qrcode/qrcode.component.ts
+++ src/app/qrcode/qrcode.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, map } from 'rxjs';
+import { interval, timer } from 'rxjs';
+import { map, takeWhile, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-qrcode',
@@ -9,25 +10,34 @@ import { interval, map } from 'rxjs';
 export class QrcodeComponent  implements OnInit {
 
   qrCodeValue: string = '';
+  countdown: number = 3; // Set initial countdown
 
   ngOnInit() {
-    // Interval untuk memperbarui QR code setiap 15 detik
+    // Timer untuk update QR code dan reset countdown setiap 15 detik
     interval(3000).pipe(
-      map(() => this.generateUniqueCode())
-    ).subscribe(code => {
-      this.qrCodeValue = code;
-    });
-    // Set pertama kali ketika komponen dimuat
+      map(() => this.generateUniqueCode()),
+      switchMap(code => {
+        this.qrCodeValue = code;
+        return this.startCountdown();
+      })
+    ).subscribe();
+
+    // Set pertama kali QR code saat komponen dimuat
     this.qrCodeValue = this.generateUniqueCode();
+    this.startCountdown().subscribe();
   }
 
-  constructor() {}
-
-   // Fungsi untuk menghasilkan kode unik
-   generateUniqueCode(): string {
-    // Menggunakan timestamp sebagai contoh, bisa diganti dengan logika lain
-    return `QR-${new Date().getTime()}`;
+  // Fungsi untuk menghasilkan kode unik
+  generateUniqueCode(): string {
+    return `QR-${new Date().getTime()}`; // Contoh: menggunakan timestamp
   }
 
+  // Fungsi untuk memulai hitungan mundur 15 detik
+  startCountdown() {
+    return timer(0, 1000).pipe(
+      map(i => 3 - i), // Hitungan mundur dari 15 ke 0
+      takeWhile(val => val >= 0), // Berhenti ketika mencapai 0
+      map(val => this.countdown = val) // Perbarui nilai countdown
+    );
+  }
 }
-
